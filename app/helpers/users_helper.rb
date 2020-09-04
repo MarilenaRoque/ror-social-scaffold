d5bd0b76de0c43ffb995f714263c10ed2b129282
Emas <emas.dev@gmail.com>
2020-09-04 10:29:30 -0500

    Fix user helper bug with show_invitation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def show_invitation(user)
-    if !current_user.friend?(user) && current_user.id != user.id?
+    if !current_user.friend?(user) && current_user.id != user.id
       link_to 'Invite to friendship',  user_friendships_path(user), method: :post, class: 'profile-link', name: 'invite'
     end
   end
